60a7e17911a2ff8ee8a16b8398673df5d0228694
Gabriel Waksmundzki <czapek1337@gmail.com>
2023-03-29 22:46:20 +0200

    Replace terminal example with drawing a line to the framebuffer

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,30 +2,41 @@
 #![no_main]
 
 use core::arch::asm;
-use limine::LimineBootInfoRequest;
 
-mod writer;
+use limine::LimineFramebufferRequest;
 
-static BOOTLOADER_INFO: LimineBootInfoRequest = LimineBootInfoRequest::new(0);
+static FRAMEBUFFER_REQUEST: LimineFramebufferRequest = LimineFramebufferRequest::new(0);
 
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
-    println!("hello, world!");
+    // Ensure we got a framebuffer.
+    if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response().get() {
+        if framebuffer_response.framebuffer_count < 1 {
+            hcf();
+        }
 
-    if let Some(bootinfo) = BOOTLOADER_INFO.get_response().get() {
-        println!(
-            "bootloader: {} v{}",
-            bootinfo.name.to_str().unwrap().to_str().unwrap(),
-            bootinfo.version.to_str().unwrap().to_str().unwrap(),
-        );
+        // Get the first framebuffer's information.
+        let framebuffer = &framebuffer_response.framebuffers()[0];
+
+        for i in 0..100_usize {
+            // Calculate the pixel offset using the framebuffer information we obtained above.
+            // We skip `i` scanlines (pitch is provided in bytes) and add `i * 4` to skip `i` pixels forward.
+            let pixel_offset = i * framebuffer.pitch as usize + i * 4;
+
+            // Write 0xFFFFFFFF to the provided pixel offset to fill it white.
+            // We can safely unwrap the result of `as_ptr()` because the framebuffer address is
+            // guaranteed to be provided by the bootloader.
+            unsafe {
+                *(framebuffer.address.as_ptr().unwrap().offset(pixel_offset as isize) as *mut u32) = 0xFFFFFFFF;
+            }
+        }
     }
 
     hcf();
 }
 
 #[panic_handler]
-fn rust_panic(info: &core::panic::PanicInfo) -> ! {
-    println!("KERNEL PANIC:\n{info}");
+fn rust_panic(_info: &core::panic::PanicInfo) -> ! {
     hcf();
 }
 
