83b3dbb436ef186d3e7dbcae6ad88b7c5e07dccb
adavis628 <adavis628@proton.me>
2024-04-03 11:40:14 -0500

    Update comments

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -8,7 +8,7 @@ use limine::BaseRevision;
 
 /// Sets the base revision to the latest revision supported by the crate.
 /// See specification for further info.
-// Be sure to mark all limine requests with #[used], otherwise they may not end up in the final binary.
+// Be sure to mark all limine requests with #[used], otherwise they may be removed by the compiler.
 #[used]
 static BASE_REVISION: BaseRevision = BaseRevision::new();
 
@@ -17,6 +17,8 @@ static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
 
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
+    // All limine requests must also be referenced in a called function, otherwise they may be
+    // removed by the linker.
     assert!(BASE_REVISION.is_supported());
 
     if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response() {
